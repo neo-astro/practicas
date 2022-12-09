4f63e3b87fedbda68f7bfb2968260b5f9d553706
astro <estevanabril@hotmail.es>
2022-12-09 17:01:14 -0500

    aqui sufriendo

---
+++ Crud/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+# class Task_Carrera(models.Model):
+#     Nom_carr
+#     Facultad
+
+# class Task_person(models.Model):
+#     N_Identificacion
+#     T_Identificacion
+#     Nombre
+#     Apellido_P
+#     Apellido_M
+#     Fecha_Nac
+#     Usu_Django
+#     Telf_Celular
+
+# class Task_Prof(Task_person):
+     
+#     Fecha_Inic
+#     Estado
+
+
+# class Task_Alum(Task_person,Task_Carrera):
+    
+#     Fecha_Inic 
+
+
+
+
